e14f99a348094b33e9f943c33345d27f22209754
逸鴻 張 <winterer32@gmail.com>
2022-12-11 22:27:10 +0800

    UI設計

--- PickACardUI/MainWindow.xaml.cs
+++ PickACardUI/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PickACardUI
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
